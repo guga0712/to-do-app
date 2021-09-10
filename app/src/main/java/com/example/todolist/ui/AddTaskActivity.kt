6f672bbc185892702f139a03129782d525f93309
guga0712 <martins0712@hotmail.com>
2021-09-10 16:44:28 -0300

    adding hour and button click listener

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -6,9 +6,13 @@ import android.util.Log
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.datasource.TaskDataSource
 import com.example.todolist.extensions.format
 import com.example.todolist.extensions.text
+import com.example.todolist.model.Task
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 class AddTaskActivity : AppCompatActivity(){
@@ -24,13 +28,41 @@ class AddTaskActivity : AppCompatActivity(){
 
     private fun insertListener() {
         binding.tilDate.editText?.setOnClickListener{
-            val datepicker =  MaterialDatePicker.Builder.datePicker().build()
-            datepicker.addOnPositiveButtonClickListener {
+            val datePicker =  MaterialDatePicker.Builder.datePicker().build()
+            datePicker.addOnPositiveButtonClickListener {
                 val timeZone = TimeZone.getDefault()
                 val offSet = timeZone.getOffset(Date().time)*-1
                 binding.tilDate.text = Date(it + offSet).format()
             }
-            datepicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+            datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+        binding.tilHour.editText?.setOnClickListener {
+            var timePicker = MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .build()
+
+            timePicker.addOnPositiveButtonClickListener{
+                val minute = if(timePicker.minute in 0..9) "0${timePicker.minute}" else timePicker.minute
+                val hour = if(timePicker.hour in 0..9) "0${timePicker.hour}" else timePicker.hour
+                binding.tilHour.text="${hour}:${minute}"
+            }
+            timePicker.show(supportFragmentManager, "TIME_PICKER_TAG")
+        }
+
+        binding.btnCancel.setOnClickListener {
+            //botao cancelar
+            finish()
+        }
+
+        binding.btnNewTask.setOnClickListener {
+            //botao de add
+            val task = Task(
+                title = binding.tilTitle.text ,
+                date = binding.tilDate.text,
+                hour = binding.tilHour.text
+            )
+            TaskDataSource.insertTask(task)
+            Log.e("TAG", "insertListener: " + TaskDataSource.getlist() )
         }
     }
 }
