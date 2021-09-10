81a99146e28c3618b78808f696dc9decfec7e91f
guga0712 <martins0712@hotmail.com>
2021-09-10 14:46:22 -0300

    adding datePicker

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -2,13 +2,35 @@ package com.example.todolist.ui
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todolist.databinding.ActivityAddTaskBinding
+import com.example.todolist.extensions.format
+import com.example.todolist.extensions.text
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.util.*
 
 class AddTaskActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityAddTaskBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityAddTaskBinding.inflate(layoutInflater)
+        binding = ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener() {
+        binding.tilDate.editText?.setOnClickListener{
+            val datepicker =  MaterialDatePicker.Builder.datePicker().build()
+            datepicker.addOnPositiveButtonClickListener {
+                val timeZone = TimeZone.getDefault()
+                val offSet = timeZone.getOffset(Date().time)*-1
+                binding.tilDate.text = Date(it + offSet).format()
+            }
+            datepicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
     }
 }
