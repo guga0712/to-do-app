0eb98abcfbec6a4d63049303908c2d58fabe3cfa
Gustavo Martins <martins0712@hotmail.com>
2021-09-10 21:10:02 -0300

    finishing recycler view

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -62,7 +62,7 @@ class AddTaskActivity : AppCompatActivity(){
                 hour = binding.tilHour.text
             )
             TaskDataSource.insertTask(task)
-            Log.e("TAG", "insertListener: " + TaskDataSource.getlist() )
+            finish()
         }
     }
 }
